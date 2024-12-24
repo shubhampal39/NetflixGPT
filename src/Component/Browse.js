d357d651e3c9989466bfec937872d6652c25de8d
unknown <shubhampal39@gmail.com>
2024-12-24 19:59:40 +0530

    Added browse page

--- src/Component/Browse.js
+++ src/Component/Browse.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { LOGO, USER_AVATAR } from '../util/constant'
 
 const Browse = () => {
   return (
-    <div>Browse</div>
+    <div className='className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10"'>
+      <img className="w-44" src={LOGO} alt="Logo" />
+      <div className='flex'>
+        <img src={USER_AVATAR} alt='avtar logo' />
+        <button>(Sign Out)</button>
+      </div>
+    </div>
   )
 }
 
