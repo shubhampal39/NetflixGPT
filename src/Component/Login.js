cbd153040f78e8f70ef7e7c936ef9156d18dc3b6
unknown <shubhampal39@gmail.com>
2025-01-15 18:49:33 +0530

    format code

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,10 +1,14 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
-import React, { useRef, useState } from 'react'
-import Header from './Header'
-import { BG_URL, USER_AVATAR } from '../util/constant'
-import { checkValidData } from '../util/validate';
-import { auth } from '../util/firebase';
-import { addUser } from '../util/userSlice';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import { BG_URL, USER_AVATAR } from "../util/constant";
+import { checkValidData } from "../util/validate";
+import { auth } from "../util/firebase";
+import { addUser } from "../util/userSlice";
 import { useDispatch } from "react-redux";
 
 const Login = () => {
@@ -17,78 +21,114 @@ const Login = () => {
 
   const toggleSignInForm = () => {
     setInSignForm(!isSignInForm);
-  }
+  };
   const handleButtonClick = () => {
     const message = checkValidData(email.current.value, password.current.value);
     setErrorMessage(message);
     if (message) return;
     if (!isSignInForm) {
-      createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
         .then((userCredential) => {
           const user = userCredential.user;
           updateProfile(user, {
-            displayName: name.current.value, photoURL: USER_AVATAR
-          }).then(() => {
-            const { uid, email, displayName, photoURL } = auth.currentUser;
-            dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }))
-            // Profile updated!
-            // navigate("/Browse");
-            // ...
-          }).catch((error) => {
-            // An error occurred
-            // ...
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            setErrorMessage(errorCode + "" + errorMessage)
-
-          });
-
+            displayName: name.current.value,
+            photoURL: USER_AVATAR,
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              // Profile updated!
+              // navigate("/Browse");
+              // ...
+            })
+            .catch((error) => {
+              // An error occurred
+              // ...
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              setErrorMessage(errorCode + "" + errorMessage);
+            });
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          setErrorMessage(errorCode + "" + errorMessage)
+          setErrorMessage(errorCode + "" + errorMessage);
         });
-    }
-    else {
-      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
         .then((userCredential) => {
           const user = userCredential.user;
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          setErrorMessage(errorCode + "" + errorMessage)
-
+          setErrorMessage(errorCode + "" + errorMessage);
         });
     }
-  }
+  };
   return (
     <div>
       <Header />
       <div className="absolute">
         <img src={BG_URL} alt="img" />
       </div>
-      <form onSubmit={(e) => e.preventDefault()} className="w-3/12 absolute p-10 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80 ">
-        <h1 className='font-bold text-3xl py-4'>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-10 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80 "
+      >
+        <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
-        {
-          !isSignInForm &&
-          <input ref={name} type="text" placeholder="Enter Name" className='p-4 my-4 w-full bg-gray-800' />
-        }
-        <input ref={email} type="text" placeholder="Enter Email address" className='p-4 my-4 w-full bg-gray-800' />
-        <input ref={password} type="Password" placeholder="Enter Password" className='p-4 my-4 w-full bg-gray-800' />
-        <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p>
-        <buttton className="p-2 my-2 bg-red-700 w-full rounded-lg cursor-pointer" onClick={handleButtonClick}>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Enter Name"
+            className="p-4 my-4 w-full bg-gray-800"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Enter Email address"
+          className="p-4 my-4 w-full bg-gray-800"
+        />
+        <input
+          ref={password}
+          type="Password"
+          placeholder="Enter Password"
+          className="p-4 my-4 w-full bg-gray-800"
+        />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <buttton
+          className="p-2 my-2 bg-red-700 w-full rounded-lg cursor-pointer"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </buttton>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-          {isSignInForm ? "New to Netflix?Sign Up Now" : "Already registred? sign In Now"}
+          {isSignInForm
+            ? "New to Netflix?Sign Up Now"
+            : "Already registred? sign In Now"}
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Component/VideoBackground.js
+++ src/Component/VideoBackground.js
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import useMovieTrailer from '../Hooks/useMovieTrailer';
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../Hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-    console.log("vfdfv movieId", movieId);
-    useMovieTrailer(movieId);
-    const trailerVideo = useSelector(store => store.movies?.trailerVideo);
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  useMovieTrailer(movieId);
+  return (
+    <div className="w-screen">
+      <iframe
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?autoplay=1&mute=1"
+        }
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+};
 
-    console.log("trailerVideo", trailerVideo);
-
-    return (
-        <div className="w-screen">
-            <iframe className="w-screen aspect-video" src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?autoplay=1&mute=1"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
-        </div>
-    )
-}
-
-export default VideoBackground
+export default VideoBackground;
